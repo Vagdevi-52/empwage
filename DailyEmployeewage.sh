73b6f8ad6e2b7ac4d6bac2fb70cde71fe974e159
vagdevi-52 <dhulipalavagdevi1952@email.com>
2021-06-10 06:33:28 -0700

    Added DailyEmployeewage

---
+++ DailyEmployeewage.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+echo "Welcome to Employee Wage Computation Program"
+
+isPresent=1
+randomcheck=$((RANDOM%2))
+if [ $isPresent -eq $randomcheck ]
+then
+        emprateperHr=20
+        empHrs=8
+        salary=$(($empHrs*$emprateperHr))
+else
+        salary=0
+fi
